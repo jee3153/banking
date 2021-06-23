76132105860b3e8f399d3efd2056f21fc928c74d
ji90 <michel3153@gmail.com>
2021-06-23 17:40:58 +0100

    Data Transaction
    Added @Transactional to makePayment method.

--- src/main/java/com/example/banking/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/example/banking/service/impl/PaymentServiceImpl.java
@@ -6,14 +6,19 @@ import com.example.banking.entities.TransactionKey;
 import com.example.banking.repository.AccountRepository;
 import com.example.banking.repository.TransactionRepository;
 import com.example.banking.service.PaymentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    private static final Logger log = LoggerFactory.getLogger(PaymentServiceImpl.class);
 
     private final TransactionRepository transactionRepository;
     private final AccountRepository accountRepository;
@@ -24,7 +29,19 @@ public class PaymentServiceImpl implements PaymentService {
         this.accountRepository = accountRepository;
     }
 
+    /*
+    try {
+       create transaction
+      makePayment()
+       commit transaction
+    catch(Exception e) {
+       rollback
+    }
+     */
+
+    @Transactional
     public void makePayment(BigDecimal amount, long payee, long recipient) {
+        log.info("Starting payment");
         // generate credit & debit transaction key
         UUID uuid = UUID.randomUUID();
         TransactionKey creditKey = new TransactionKey(uuid, "CREDIT");
@@ -40,14 +57,18 @@ public class PaymentServiceImpl implements PaymentService {
         payeeAccount.takeBalance(amount);
 
         // save changes to account repository
+        log.info("1st account");
         accountRepository.save(payeeAccount);
+        log.info("2nd account");
         accountRepository.save(recipientAccount);
 
         // create debit & credit transactions
         Transaction debitTransaction = new Transaction(debitKey, amount, recipientAccount, payeeAccount);
         Transaction creditTransaction = new Transaction(creditKey, amount, payeeAccount, recipientAccount);
         // post debit & credit transactions
+        log.info("1st transaction");
         transactionRepository.save(debitTransaction);
+        log.info("2nd transaction");
         transactionRepository.save(creditTransaction);
 
     }
